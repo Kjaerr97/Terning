2929b3677546decd2f8eb15efae12737773de644
Siff <siff.ravn@gmail.com>
2018-10-07 20:30:15 +0200

    Bugfix af roll metoden 1.0 Siff

--- src/terning/Dice.java
+++ src/terning/Dice.java
@@ -1,12 +1,13 @@
 package terning;
 
+import java.util.Random;
+
 public class Dice {
 	// roll the die and return the value (1-6)
 	public int roll() {
-		float d1=(float)Math.random();     // 0-1
-		float d2=d1*5;                     // 0-5
-		int d3=Math.round(d2);             // 0-5 integer
-		return d3 + 1;                     // 1-6		
+		Random roller = new Random();
+		int faceValue = roller.nextInt(6)+1;
+		return faceValue;
 	}
 	
 	// roll the die n times and print the values
