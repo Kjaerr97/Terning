dd1f68a45b111a67e7e467616cd67607764ca707
Gustav Vang Klinker <gulle139@hotmail.com>
2018-10-08 10:39:49 +0200

    New roll method

--- src/terning/Dice.java
+++ src/terning/Dice.java
@@ -1,14 +1,17 @@
 package terning;
 
+import java.util.Random;
 public class Dice {
-
+	Random random = new Random();
 
 	// roll the die and return the value (1-6)
 	public int roll() {
-		float d1=(float)Math.random();     // 0-1
+		/*float d1=(float)Math.random();     // 0-1
 		float d2=d1*5;                     // 0-5
 		int d3=Math.round(d2);             // 0-5 integer
-		return d3 + 1;                     // 1-6		
+		return d3 + 1;                     // 1-6		*/
+
+		return random.nextInt(6) + 1;
 	}
 	
 	// roll the die n times and print the values
